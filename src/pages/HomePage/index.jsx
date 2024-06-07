6eb714b1d2b2dfddc175176f5b69f3ce357d500e
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-07 16:12:08 +0500

    skjdnfkd

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,15 +1,42 @@
 import clsx from "clsx";
 import cn from './style.module.scss'
+import Card from "../../components/Card";
 
 
 
 export default function HomePage() {
 
+    let a = {
+        id: '1',
+        matn: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, vero suscipit magnam autem commodi voluptatibus corporis quidem deserunt accusamus labore qui, obcaecati nesciunt ea placeat, odit fugit facere illum sit eius vitae reiciendis expedita eveniet architecto eum. Commodi, quod pariatur? Doloremque ipsa nulla, accusamus magni, est officiis illum vitae excepturi quibusdam quisquam consequatur distinctio ullam quas aliquid soluta quos accusantium unde pariatur laudantium. Sapiente possimus et ratione maxime dolorum quisquam pariatur architecto accusantium voluptatum voluptatibus dolores consectetur, voluptate magnam molestias eum deserunt nam corporis blanditiis. Quam culpa, nulla distinctio rem dignissimos eum, quos dolorum magni repudiandae velit a, ullam incidunt odio?",
+        time: '12;40',
+        do: 'true'
+    }
+
+    function doit(index) {
+        a.do = 'true';
+        console.log(a.do);
+    }
+    function doitagain(index) {
+        a.do = 'false';
+        console.log(a.do);
+    }
+
+    function del(index) {
+        console.log("del");
+        console.log(index);
+    }
+    function edit(index) {
+        console.log("edit");
+        console.log(index);
+    }
     return (
         <>
         <div className={clsx(cn['DOM'])}>
       
             <h1>To-Do-List</h1>
+
+            <Card e= {a} del = {del} edit={edit} doit={doit} doitagain={doitagain}></Card>
 
         </div>  
         </>
