de84088c1c7ef3034b44ef5d10b2752233bef0db
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-07 13:46:15 +0500

    homepage

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,18 @@
+import clsx from "clsx";
+import cn from './style.module.scss'
+
+
+
+export default function HomePage() {
+
+    return (
+        <>
+        <div className={clsx(cn['DOM'])}>
+      
+            <h1>To-Do-List</h1>
+
+        </div>  
+        </>
+    )
+    
+}
