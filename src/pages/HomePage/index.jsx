6b861b5025356608242aa364b724cc7530406a43
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-07 19:29:06 +0500

    homepage tugadi

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,35 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+
 import clsx from "clsx";
 import cn from './style.module.scss'
 import Card from "../../components/Card";
+import Typography from "../../components/Typography";
 
 
 
 export default function HomePage() {
-    let todos = JSON.parse(localStorage.getItem("todo"));
+    // localStorage.clear();
+    let todos = JSON.parse(localStorage.getItem("todos"));
     if(todos == null){
-        todos = [{}];
+        todos = [];
+        console.log("bo'sh");
     }
-    
+    const [arr, setArr] = useState(todos);
+    const [num, setNum] = useState(1);
 
-    let a = {
-        id: '1',
-        matn: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, vero suscipit magnam autem commodi voluptatibus corporis quidem",
-        time: '12;40',
-        do: true
-    }
+    console.log(arr);
+    let input = useRef();
+    useEffect(()=>{
+        console.log('Ozgardi');
+
+        return ()=>{
+            console.log('cleanup');
+        }
+    },[num, arr])
 
     function doit(index) {
-        a.do = 'true';
-        console.log(a.do);
+        // a.do = 'true';
+        console.log(index);
     }
     function doitagain(index) {
-        a.do = 'false';
-        console.log(a.do);
+        // a.do = 'false';
+        console.log(index);
     }
-
     function del(index) {
         console.log("del");
         console.log(index);
+        let aaa = arr.filter((e)=>{
+            if(e.id !== index) return e;
+        })
+        console.log(aaa);
+          setArr(aaa);
+        console.log(aaa);
+        localStorage.setItem('todos', JSON.stringify(aaa));
     }
     function edit(index) {
         console.log("edit");
@@ -38,12 +53,39 @@ export default function HomePage() {
     return (
         <>
         <div className={clsx(cn['DOM'])}>
-      
+
+
+
             <h1>To-Do-List</h1>
+            {/* <Typography tag='h1' classname='main'>To-Do-List</Typography> */}
+            <form action="" onSubmit={(e)=>{
+                e.preventDefault();
+                let soat = new Date().getHours();
+                let minut = new Date().getMinutes();
+                console.log(soat, minut);
+                let obj = {
+                    id: todos[todos.length-1].id+1,
+                    text : input.current.value,
+                    time: `${soat}:${minut}`,
+                    do: false
+                };
+                console.log(obj);
+                todos.push(obj);
+                localStorage.setItem("todos", JSON.stringify(todos));
+                setArr(todos);
+                console.log(arr);
+            }}>
+                <input type="text" placeholder="Enter todo" ref={input} />
+                <button>Submit</button>
+            </form>
 
             <div className={clsx(cn['cards'])}>
+                {
+                    arr.map((e)=>{
+                       return <Card e= {e} del = {del} edit={edit} doit={doit} doitagain={doitagain}></Card>
+                    })
+                }
                 
-                <Card e= {a} del = {del} edit={edit} doit={doit} doitagain={doitagain}></Card>
             </div>
         </div>  
         </>
